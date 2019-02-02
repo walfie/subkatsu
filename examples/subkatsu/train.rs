ce93ae8088c607fd9c7096f33d0fad2af5e80bd0
Walfie <walfington@gmail.com>
2019-02-02 12:12:48 -0500

    Fix regex for transparency
    Forgot that color codes can be lowercase as well, or only 2-characters
    long, in the case where only alpha is changing. `\h` should also be
    replaced with a space, rather than empty string.

--- examples/subkatsu/train.rs
+++ examples/subkatsu/train.rs
@@ -1,22 +1,33 @@
 use crate::opts;
 use log::*;
 
+fn tokenize(text: &str) -> impl Iterator<Item = &str> {
+    // TODO: Detect language, handle punctuation, case-sensitivity, etc
+    let tokens = text.split(' ').filter_map(|string| match string.trim() {
+        "" => None,
+        s => Some(s),
+    });
+
+    tokens
+}
+
 pub fn train(args: opts::Train) -> Result<(), Box<std::error::Error>> {
     let mut chain = markov::Chain::of_order(args.order);
 
     // {\c&H........&} changes color. If the alpha starts with F, the text
     // is transparent, so we should exclude any text afterward, until the
     // next color change, or end of line.
+    // TODO: Should probably stop trying to clean the subs in code, and
+    // just assume the subtitle files themselves are dialogue-only.
     let escapes = regex::Regex::new(
         r"(?x)                          # Whitespace-insignificant mode, for comments
-        ( \{\\c&HF.{7}&}.*(\{.+?}|$)
+        ( \{.*\\c&H(F|f).(.{6})?&.*}.*(\{.*\\(c|r).+?}|$)
         | \{.+?}                        # Anything between {}'s is a comment
-        | \\h                           # Non-breaking space
         )
         ",
     )?;
 
-    let newlines = regex::Regex::new(r"\\N|\\n")?;
+    let spaces = regex::Regex::new(r"\\N|\\n|\\h")?;
 
     for path in args.input {
         let format = subparse::get_subtitle_format_by_ending_err(&path)?;
@@ -30,15 +41,9 @@ pub fn train(args: opts::Train) -> Result<(), Box<std::error::Error>> {
         for entry in subs {
             if let Some(line) = entry.line {
                 let line = escapes.replace_all(&line, "");
-                let line = newlines.replace_all(&line, " ");
+                let line = spaces.replace_all(&line, " ");
 
-                let tokens = line
-                    .split(' ')
-                    .filter_map(|string| match string.trim() {
-                        "" => None,
-                        s => Some(s.to_owned()),
-                    })
-                    .collect();
+                let tokens = tokenize(&line).map(String::from).collect::<Vec<_>>();
                 chain.feed(tokens);
             }
         }
