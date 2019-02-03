665ad52f62c91513a12aa870c6458ecac1582305
Walfie <walfington@gmail.com>
2019-02-03 16:20:25 -0500

    Add more detail to help text

--- src/opts.rs
+++ src/opts.rs
@@ -24,26 +24,29 @@ pub struct Train {
 
     #[structopt(
         long = "order",
-        default_value = "1",
-        help = "Order of the Markov model"
+        default_value = "2",
+        help = "Order of the Markov model. Higher values cause the generated \
+                text to more closely resemble the training set."
     )]
     pub order: usize,
 
     #[structopt(
         required = true,
-        help = "Training files to use as input (.srt/.ssa/.ass)"
+        help = "List of training files to use as input \
+                (should have extensions .srt/.ssa/.ass)"
     )]
     pub input: Vec<String>,
 }
 
 #[derive(Debug, StructOpt)]
 pub struct Generate {
-    #[structopt(help = "A model file generated from the training phase")]
+    #[structopt(help = "Path to a model file generated from the training phase")]
     pub model: String,
 
     #[structopt(
         short = "n",
-        default_value = "10",
+        long = "count",
+        default_value = "25",
         help = "Number of chains to generate"
     )]
     pub count: u32,
@@ -51,13 +54,14 @@ pub struct Generate {
     #[structopt(
         long = "start-token",
         alias = "start",
-        help = "Generate chains starting with this token"
+        help = "Generate chains starting with this token. \
+                Note that this will only work if the model was trained with order = 1."
     )]
     pub start: Option<String>,
 
     #[structopt(
         long = "min-length",
-        help = "Generated chains must have at least this many characters"
+        help = "Ensure that generated chains have at least this many characters"
     )]
     pub min_length: Option<usize>,
 }
