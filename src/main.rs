eead785a8ba66c1d15f1930634ecb62d96b48088
Walfie <walfington@gmail.com>
2019-02-09 15:26:59 -0500

    Manually drop logger on error
    This avoids needing a weird multi-function setup in `main`

--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,31 @@ pub mod error;
 pub mod generate;
 pub mod opts;
 
-use error::*;
 use opts::Opts;
-use slog::{Drain, Logger};
+use slog::Drain;
 use structopt::StructOpt;
 
 fn main() {
-    let exit_code = run();
-
-    std::process::exit(exit_code)
-}
-
-// This separate method is needed for slog_async to flush properly
-fn run() -> i32 {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
 
     let log = slog::Logger::root(drain, slog::o!());
 
-    if let Err(err) = try_run(&log) {
+    let result = match Opts::from_args() {
+        Opts::Train(args) => train::train(&log, args),
+        Opts::Generate(args) => generate::generate(&log, args),
+    };
+
+    if let Err(err) = result {
         slog::error!(log, "Encountered error"; "description" => %err);
 
         for cause in err.iter().skip(1) {
             slog::error!(log, "Underlying error"; "description" => %cause);
         }
 
-        return 1;
+        // Drop the logger so the messages get flushed before exiting
+        std::mem::drop(log);
+        std::process::exit(1);
     }
-
-    return 0;
-}
-
-fn try_run(log: &Logger) -> Result<()> {
-    match Opts::from_args() {
-        Opts::Train(args) => train::train(log, args)?,
-        Opts::Generate(args) => generate::generate(log, args)?,
-    }
-
-    Ok(())
 }
