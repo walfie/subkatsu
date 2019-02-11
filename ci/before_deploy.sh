aebe7818fe485dfdc11bbbba5a535fc71182532c
Walfie <walfington@gmail.com>
2019-02-11 00:00:51 -0500

    Add CI scripts

---
+++ ci/before_deploy.sh
@@ -0,0 +1,34 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # Build artifacts
+    cross rustc --bin $EXECUTABLE_NAME --target $TARGET --release -- -C lto
+
+    # Package artifacts
+    cp target/$TARGET/release/$EXECUTABLE_NAME $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
+
